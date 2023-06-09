97c9a6b7bd62eec0a0a67995bcb325de4d3379bc
damienchoojl <damienchoojl@gmail.com>
2023-06-09 15:21:21 +0800

    Crypto Exchange
    added Input and Search Button on HomePage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./Data/Components/HomePage";
-import CryptoDetailsPage from "./Data/Components/CryptoDetailsPage";
-import ChartPage from "./Data/Components/ChartPage";
+import HomePage from "./Components/HomePage";
+import CryptoDetailsPage from "./Components/CryptoDetailsPage";
+import ChartPage from "./Components/ChartPage";
 
 export default function App() {
   return (
--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,3 +1,21 @@
+import React, { useState } from "react";
+
 export default function HomePage() {
-  return <p>HomePage</p>;
+  const [cryptoInput, setCryptoInput] = useState("");
+
+  const handleCryptoSearch = async () => {
+    console.log("searching");
+  };
+
+  return (
+    <>
+      Crypto Exchange:{" "}
+      <input
+        type="text"
+        value={cryptoInput}
+        onChange={(e) => setCryptoInput(e.target.value)}
+      />
+      <button onClick={handleCryptoSearch}>Search</button>
+    </>
+  );
 }
