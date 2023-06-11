cb70574ba41af4215e1e756ebcef3841734ba7bb
damienchoojl <damienchoojl@gmail.com>
2023-06-11 15:59:10 +0800

    Crypto Exchanges
    updated search button features on HomePage.jsx

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,19 +1,32 @@
 import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function HomePage() {
   const [cryptoInput, setCryptoInput] = useState("");
+  const [cryptoList, setCryptoList] = useState([]);
+
+  const navigate = useNavigate();
 
   const handleCryptoSearch = async () => {
-    console.log("searching");
-    const response = await fetch(
-      `https://api.coingecko.com/api/v3/exchanges/${cryptoInput.toLowerCase()}`
-    );
-    const data = await response.json();
-    console.log(data);
+    try {
+      const response = await fetch(
+        `https://api.coingecko.com/api/v3/exchanges/${cryptoInput.toLowerCase()}`
+      );
+      const data = await response.json();
+      if (data) {
+        setCryptoList([data]);
+      } else {
+        setCryptoList([]);
+      }
+    } catch (error) {}
+  };
+
+  const handleCryptoClick = (id) => {
+    navigate(`/exchanges/${id}`);
   };
 
   return (
-    <form>
+    <>
       Crypto Exchange:{" "}
       <input
         type="text"
@@ -22,6 +35,16 @@ export default function HomePage() {
         onChange={(e) => setCryptoInput(e.target.value)}
       />
       <button onClick={handleCryptoSearch}>Search</button>
-    </form>
+      {cryptoList.map((crypto) => (
+        <Link
+          to={`/exchanges/${cryptoInput}`}
+          key={crypto.id}
+          style={{ display: "block" }}
+          onClick={() => handleCryptoClick(crypto.id)}
+        >
+          {crypto.name}
+        </Link>
+      ))}
+    </>
   );
 }
